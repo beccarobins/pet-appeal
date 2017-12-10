2ab7bec5a4c9627e40c35db15c3b9c05bac8ac8e
Becca Robins <becca.k.robins@gmail.com>
2017-12-10 12:42:55 -0500

    Add files via upload

--- 1_petAppeal_shelterFinder.py
+++ 1_petAppeal_shelterFinder.py
@@ -1,140 +1,31 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-
-import json
-import sys
 import pandas as pd
-import urllib
-import numpy as np
+import petAppeal
 
-#Pet Finder API key requested: 9.15.2017
-petFinder_api_key = '1baf2f363bd60991ac552f9168cbaec0'
-petFinder_api_secret = '7465719af394876d7a509000e69c673a'
+##Request api key and api secret from https://www.petfinder.com/developers/api-docs
+petFinder_api_key = ''
 
-##Pet Finder API key requested: 9.16.2017
-petFinder_api_key = '49eacc3915af403849e24c8ec3e62b20'
-petFinder_api_secret = '4eac7b0518bfd9552f79e85328035b09'
+##This uses the scraped No Kill Network to query for animal shelters
+##Any list of (US) zip codes can be used for querying
+url = 'https://raw.githubusercontent.com/beccarobins/PetAppeal/master/No%20Kill%20Network%20Animal%20Shelters.csv'
+zip_code_list = pd.read_csv(url)
+zip_code_list = zip_code_list.shelter_zip_code.dropna(axis=0).reset_index().drop(labels=['index'], axis=1)
 
-##see https://www.petfinder.com/developers/api-docs for shelter.find method arguments
+shelters = pd.DataFrame()
 
-def shelterFinder(zipcode):
-    url = 'http://api.petfinder.com/shelter.find?key='+petFinder_api_key+'&location='+zipcode+'&format=json'
-    ##sample url: url = 'https://www.googleapis.com/customsearch/v1?q=+cat+filetype:jpg+solid+brown&cx='+googleCustomSearch_CSE_ID+'&searchType=image&key='+googleCustomSearch_api_key+'&num='+num_images+'&fields=items%2Flink'
-    print url
-
-    try:
-        json_obj = urllib.urlopen(url)
-        data = json.load(json_obj)
-
-        individual_shelters = data['petfinder']['shelters']['shelter']
-        for i in range(len(individual_shelters)):
-            shelter_info = individual_shelters[i]
-            try:
-                address1.append(shelter_info['address1']['$t'].encode("utf-8"))
-            except:
-                address1.append(np.nan)
-                print 'Address1 blank'
-            try:
-                address2.append(shelter_info['address2']['$t'].encode("utf-8"))
-            except:
-                address2.append(np.nan)
-                print 'Address2 blank'
-            try:
-                city.append(shelter_info['city']['$t'].encode("utf-8"))
-            except:
-                city.append(np.nan)
-                print 'City not provided'
-            try:
-                country.append(shelter_info['country']['$t'].encode("utf-8"))
-            except:
-                country.append(np.nan)
-                print 'City not provided' 
-            try:
-                email.append(shelter_info['email']['$t'].encode("utf-8"))
-            except:
-                email.append(np.nan)
-                print 'E-mail not provided'
-            try:
-                fax.append(shelter_info['fax']['$t'].encode("utf-8"))
-            except:
-                fax.append(np.nan)
-                print 'Who uses fax machines?'
-            try:
-                shelter_id.append(shelter_info['id']['$t'].encode("utf-8"))
-            except:
-                shelter_id.append(np.nan)
-                print 'No shelter id'
-            try:
-                latitude.append(shelter_info['latitude']['$t'].encode("utf-8"))
-            except:
-                latitude.append(np.nan)
-                print 'No shelter id'
-            try:
-                longitude.append(shelter_info['longitude']['$t'].encode("utf-8"))
-            except:
-                longitude.append(np.nan)
-                print 'No shelter id'
-            try:
-                name.append(shelter_info['name']['$t'].encode("utf-8"))
-            except:
-                name.append(np.nan)
-                print 'No name provided'     
-            try:
-                phone.append(shelter_info['phone']['$t'].encode("utf-8"))
-            except:
-                phone.append(np.nan)
-                print 'Phone number not provided'
-            try:
-                state.append(shelter_info['state']['$t'].encode("utf-8"))
-            except:
-                state.append(np.nan)
-                print 'State not listed'
-            try:
-                zip_code.append(shelter_info['zip']['$t'].encode("utf-8"))
-            except:
-                zip_code.append(np.nan)
-                print 'Zip code not listed'
-    except:
-        print "Oops!",sys.exc_info(),"occured"
+for i in range(0, len(zip_code_list)):
     
-    return address1, address2, city, country,  email, fax, shelter_id, phone, latitude, longitude, name, phone, state, zip_code
-
-
-file = '/home/becca/Insight Project/data files/no_kill_shelters.csv'
-no_kill_shelters = pd.read_csv(file)
-no_kill_shelters = no_kill_shelters.drop(labels=['Unnamed: 0'], axis=1)
-
-zipcodes_to_check = no_kill_shelters.drop_duplicates(subset=['zip_code'])
-
-address1 = []
-address2 = []
-city = []
-country = []
-email = []
-fax = []
-shelter_id = []
-phone = []
-latitude = []
-longitude = []
-name = []
-phone = []
-state = []
-zip_code = []
-   
-for i in range(len(zipcodes_to_check)):
-    print i, 'of', len(zipcodes_to_check)
-    zipcode = str(zipcodes_to_check.zip_code[i])
-    shelterFinder(zipcode)
-
-
-found_shelters = pd.DataFrame({'address1': address1, 'address2': address2,
-                               'city': city, 'country': country, 'email': email,
-                               'fax': fax, 'shelter_id': shelter_id, 'phone': phone,
-                               'latitude': latitude, 'longitude': longitude,
-                               'name': name, 'phone': phone, 'state': state, 'zip_code': zipcode})
-  
-found_shelters = found_shelters.drop_duplicates(subset=['shelter_id'])
-found_shelters = found_shelters.reset_index()
-found_shelters = found_shelters.drop(['index'], axis = 1)
+    zipcode = str(int(zip_code_list.iloc[i]))
     
-found_shelters.to_csv('/home/becca/Insight Project/data files/no_kill_shelters_detailed.csv', sep=',', na_rep='', float_format=None, columns=None, header=True, index=True, index_label=None, mode='w', encoding=None, compression=None, quoting=None, quotechar='"', line_terminator='\n', chunksize=None, tupleize_cols=False, date_format=None, doublequote=True, escapechar=None, decimal='.')
+    while len(zipcode)<5:
+        zipcode = '0'+zipcode
+    
+    shelters = shelters.append(petAppeal.shelterFinder(zipcode, petFinder_api_key))
+
+shelters = shelters.drop_duplicates(subset=['shelter_id'])
+shelters = shelters.reset_index().drop(['index'], axis = 1)
+
+##Add the file path where the shelter list should be saved
+local_file_path = ''
+petfinder_file = local_file_path + 'petfinder_shelter_list'
+
+shelters.to_csv(petfinder_file)
